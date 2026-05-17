e1066dd1db85bc12ba3aa8449ddf583540d1c0d5
Tanuj Nayanam <nayatanuj@gmail.com>
2026-05-17 14:27:46 -0500

    passing function in prop notify parent of a change

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,11 +2,13 @@ import Messages from "./Message";
 import LG from "./components/ListGroup";
 
 function App() {
+  const handleSelectedItem = (item: string) => console.log(item);
+
   const items = ["New York", "India", "Delhi", "Columbus"];
 
   return (
     <div>
-      <LG heading="Cities" items={items} />
+      <LG heading="Cities" items={items} onSelectItem={handleSelectedItem} />
     </div>
   );
 }
--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -3,9 +3,10 @@ import { MouseEvent, useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
@@ -22,7 +23,10 @@ function ListGroup({ items, heading }: Props) {
                 : "list-group-item"
             }
             key={item}
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>

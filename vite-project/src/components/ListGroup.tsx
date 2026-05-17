916da176020770883e661fe134e7fbe2c0ba04a4
Tanuj Nayanam <nayatanuj@gmail.com>
2026-05-17 11:32:34 -0500

    on click event as function reference

--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -11,12 +11,7 @@ function ListGroup() {
       {items.length === 0 && <p> No list element found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          //   <li className="list-group-item" key={item} onClick={handleClick}>
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={(event) => console.log(event)}
-          >
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}

5f5937ff9fc4e8030cadb1dbcfb0393cc2e79c6f
Tanuj Nayanam <nayatanuj@gmail.com>
2026-05-17 21:22:06 -0500

    vanilla javascript

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,18 +3,19 @@ import Messages from "./Message";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import LG from "./components/ListGroup";
+import ListGroup from "./components/ListGroup";
 
 function App() {
   const [setAlert, setAlertVisibility] = useState(false);
+  const items = ["New York", "India"];
 
   return (
     <div>
-      {setAlert && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button color="danger" handleClick={() => setAlertVisibility(true)}>
-        My Button
-      </Button>
+      <ListGroup
+        items={items}
+        heading="menu"
+        onSelectItem={() => console.log("clicked")}
+      ></ListGroup>
     </div>
   );
 }

77b6e7bcab51fec21c125adb6769115c6fbc0276
Tanuj Nayanam <nayatanuj@gmail.com>
2026-05-18 18:35:12 -0500

    controlled component switching from ref hook to useState

--- vite-project/src/components/Form.tsx
+++ vite-project/src/components/Form.tsx
@@ -1,19 +1,15 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = {
+  const [person, setPerson] = useState({
     name: "",
     age: 0,
-  };
+  });
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (nameRef.current != null) person.name = nameRef.current.value;
-        if (ageRef.current != null) person.age = parseInt(ageRef.current.value);
         console.log(person);
       }}
     >
@@ -21,13 +17,29 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          value={person.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit

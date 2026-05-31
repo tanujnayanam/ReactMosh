91de50bafff04ddb017b3eea36ff0923f97a3038
Tanuj Nayanam <nayatanuj@gmail.com>
2026-05-31 18:40:29 -0500

    loading loader on data fetch

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -9,22 +9,29 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
     return () => controller.abort();
   }, []);
   return (
     <>
+      {isLoading && <div className="spinner-border" />}
       {error && <p className="text-danger">{error}</p>}
       <div>
         <ul>
